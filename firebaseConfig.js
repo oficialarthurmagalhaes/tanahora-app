856dcf76c7b18647ed064dd04c11963a848a16a3
Arthur Magalhães <oficialarthurmagalhaes@gmail.com>
2026-04-15 21:50:32 -0300

    Criação de conexão com o firebase

---
+++ firebaseConfig.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDZZIcArLELSAb_x2lAxKWlXPGkDHGyamc",
+  authDomain: "app-tanahora.firebaseapp.com",
+  projectId: "app-tanahora",
+  storageBucket: "app-tanahora.firebasestorage.app",
+  messagingSenderId: "1094063141262",
+  appId: "1:1094063141262:web:5201815b181a87dc1bb4df",
+  measurementId: "G-SSBMBW7WKZ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
